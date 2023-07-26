3cfccde485a43ef134421edb9363315a6159e983
tim <1536576466@qq.com>
2023-07-26 23:44:13 +0800

    test tailwindcss

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
     <!-- <NuxtWelcome /> -->
-    <h1>Hello Nuxt3!!!</h1>
+    <h1 class="text-4xl text-green-700 text-center font-semibold">Hello Nuxt3!!!</h1>
   </div>
 </template>
