4d3334cd95b79d386f56c4eea6e5d3a6eccbd8f7
tim <1536576466@qq.com>
2023-08-01 22:40:55 +0800

    add some page

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <!-- <NuxtWelcome /> -->
-    <h1 class="mt-10px text-4xl text-green-700 text-center font-semibold">Hello Nuxt3!!!</h1>
+    <NuxtPage />
+    <!-- <h1>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAa</h1> -->
   </div>
 </template>
