820c151c0fbcb96cf39c1f6665f6e7dc5670cde0
tim <1536576466@qq.com>
2023-08-02 23:06:19 +0800

    unocss

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-<h1>index page!!!</h1>
+<h1 class="p-10">index page!!!</h1>
 </template>
 <script setup lang='ts'>
 </script>
--- uno.config.ts
+++ uno.config.ts
@@ -8,4 +8,8 @@ export default defineConfig({
     presetAttributify(),
     presetRemToPx(),
   ],
+  rules: [
+  [/^m-(\d+)$/, ([, d]) => ({ margin: `${d / 4}rem` })],
+  [/^p-(\d+)$/, match => ({ padding: `${match[1] / 4}rem` })],
+]
 })
