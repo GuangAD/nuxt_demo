0e38ae4be461e0a793db56febf6f56d90e8cdc32
tim <1536576466@qq.com>
2023-08-08 22:47:59 +0800

    add daisy

---
+++ parseUnocss/daisy/test.js
@@ -0,0 +1,11 @@
+const fs = require('fs')
+const path = require('path')
+
+  ; (async () => {
+    const { presetDaisy: preset } = await import('unocss-preset-daisy')
+    console.log(preset);
+    const presetcontent = JSON.stringify(preset())
+    fs.writeFileSync(path.join(__dirname, './presetcontent.json'), presetcontent, (error) => {
+      console.log(error);
+    })
+  })()
