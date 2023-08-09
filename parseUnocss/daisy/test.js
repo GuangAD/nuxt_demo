fc565cd6349589b7581254202882c2d2eccade4e
tim <1536576466@qq.com>
2023-08-09 22:46:17 +0800

    code

--- parseUnocss/daisy/test.js
+++ parseUnocss/daisy/test.js
@@ -4,7 +4,10 @@ const path = require('path')
   ; (async () => {
     const { presetDaisy: preset } = await import('unocss-preset-daisy')
     console.log(preset);
-    const presetcontent = JSON.stringify(preset())
+    const presetcontent = JSON.stringify(preset({
+      styled: false,
+      themes: ['light', 'dark'],
+    }))
     fs.writeFileSync(path.join(__dirname, './presetcontent.json'), presetcontent, (error) => {
       console.log(error);
     })
